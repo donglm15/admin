78685c9aef249b065eebac56cac50bcf8a9e43f2
linzh36 <717557150@qq.com>
2019-05-22 20:25:39 +0800

    活动后端0.03

--- src/main/java/com/unicom/admin/controller/ActivityController.java
+++ src/main/java/com/unicom/admin/controller/ActivityController.java
@@ -6,6 +6,7 @@ import com.github.pagehelper.PageInfo;
 import com.unicom.admin.model.Activity;
 import com.unicom.admin.model.JSONResult;
 import com.unicom.admin.service.ActivityService;
+import io.swagger.annotations.*;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "activity")
+@Api(tags = "联通之家-活动管理模块")
 public class ActivityController {
 
     @Autowired
     ActivityService activityService;
 
-    @RequestMapping(value = "list")
+    @GetMapping(value = "list")
+    @ApiOperation(value = "获取活动列表")
     public JSONResult getAllActivity(){
         List<Activity> list = activityService.getAllActivity();
         return new JSONResult().ok(list);
@@ -31,6 +34,16 @@ public class ActivityController {
 
 
     @GetMapping(value = "select")
+    @ApiOperation(value = "后台分页获取活动列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "分页页码", required = true,dataType = "int"),
+            @ApiImplicitParam(name = "limit", value = "每页显示条数", required = true,dataType = "int"),
+            @ApiImplicitParam(name = "title", value = "按活动名称搜索关键字",dataType = "string"),
+            @ApiImplicitParam(name = "type", value = "活动状态",dataType = "string"),
+            @ApiImplicitParam(name = "startTime", value = "活动开始时间",dataType = "string"),
+            @ApiImplicitParam(name = "endTime", value = "活动结束时间",dataType = "string"),
+            @ApiImplicitParam(name = "order", value = "活动排序", dataType = "string"),
+    })
     public JSONResult select(
             @RequestParam(value = "page", defaultValue = "1") Integer page,
             @RequestParam(value = "limit", defaultValue = "10") Integer limit,
@@ -47,6 +60,8 @@ public class ActivityController {
         return new JSONResult().ok(pageInfo);
     }
 
+    @ApiOperation(value = "根据活动id删除活动")
+    @ApiImplicitParam(name = "id",value = "活动id",required = true,dataType = "int")
     @GetMapping(value = "delete")
     public JSONResult deleteActivity(@RequestParam(name = "id") int id){
         activityService.deleteActivity(id);
@@ -54,14 +69,23 @@ public class ActivityController {
     }
 
 
+    @ApiOperation(value = "插入活动")
+    @ApiImplicitParam(name = "activity", value = "活动数据", required = true, dataType = "Activity")
     @PostMapping(value = "insert")
     public JSONResult insertActivity(@RequestBody Activity activity){
         activityService.insertActivity(activity);
         return new JSONResult().ok("新增成功");
     }
 
+    @ApiOperation(value = "更新活动")
     @GetMapping(value = "update")
-    public JSONResult updateActivity(@Param(value = "people") String people, @Param(value = "prize") String prize, @Param(value = "title") String title, @Param(value = "content") String content, @Param(value = "startTime") String startTime, @Param(value = "endTime") String endTime, @Param(value = "id") int id){
+    public JSONResult updateActivity(@RequestParam(value = "people") @ApiParam(value = "活动人数", required = true) String people,
+                                     @RequestParam(value = "prize") @ApiParam(value = "活动奖品", required = true) String prize,
+                                     @RequestParam(value = "title") @ApiParam(value = "活动名称", required = true) String title,
+                                     @RequestParam(value = "content") @ApiParam(value = "活动内容", required = true) String content,
+                                     @RequestParam(value = "startTime") @ApiParam(value = "活动开始时间", required = true) String startTime,
+                                     @RequestParam(value = "endTime") @ApiParam(value = "活动结束时间", required = true) String endTime,
+                                     @RequestParam(value = "id") @ApiParam(value = "活动id", required = true) int id){
         try {
             activityService.updateActivity(people,prize,title,content,new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(startTime),new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(endTime),id);
         } catch (ParseException e) {
